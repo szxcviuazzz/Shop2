b4f5f1ea335b17ff19c4e1af6e29abb0630d8496
trtran <kiemkhach91123@outlook.com.vn>
2025-07-15 13:32:00 +0700

    tao la cuop day 2

---
+++ shopp2/src/main/java/com/example/shopp2/Shopp2Application.java
@@ -0,0 +1,13 @@
+package com.example.shopp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shopp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shopp2Application.class, args);
+	}
+
+}
---
+++ shopp2/src/test/java/com/example/shopp2/Shopp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shopp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
